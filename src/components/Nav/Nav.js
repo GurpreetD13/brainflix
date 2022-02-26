df75b6343469c2abb7c01dbb6bc6642ec84a80df
Gurpreet <gurpreeet.dhaliwal13@gmail.com>
2022-02-26 17:30:33 -0500

    Created html for Nav component

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,12 +1,18 @@
 import './Nav.scss';
+import logo from '../../assets/logos/BrainFlix-logo.svg';
 
 const Nav = () => {
     return (
 
-        <>
-            <p>Nav</p>
-        </>
-
+        <nav className='nav'>
+            <img className='nav__logo' src={logo} alt="brainflix logo" />
+            <form>
+                <input type="text" placeholder='Search' />
+            </form>
+            {/* change button type to text */}
+            <button disabled="disabled">UPLOAD</button>
+            <div className='nav__avatar'></div>
+        </nav>
     );
 };
 
