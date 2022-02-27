9ee36bff78e62df447d0f6bb303695c01906fd33
Gurpreet <gurpreeet.dhaliwal13@gmail.com>
2022-02-26 20:36:00 -0500

    Styeld avatar and completed first draft of Nav

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -3,14 +3,14 @@ import logo from '../../assets/logos/BrainFlix-logo.svg';
 
 const Nav = () => {
     return (
-
         <nav className='nav'>
-            <img className='nav__logo' src={logo} alt="brainflix logo" />
+            <img className='nav__logo' src={logo} alt="BrainFlix logo" />
             <form>
                 <input className='nav__search' type="text" placeholder='Search' />
+                {/* <div className='nav__avatar avatar nav__avatar--mobile'></div> */}
             </form>
             <button>UPLOAD</button>
-            <div className='nav__avatar'></div>
+            <div className='nav__avatar avatar nav__avatar--tablet'></div>
         </nav>
     );
 };
