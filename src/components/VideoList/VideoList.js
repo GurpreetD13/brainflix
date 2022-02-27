3c7674cd76f6a067b4d1fc2128e9094f63c5bcc9
Gurpreet <gurpreeet.dhaliwal13@gmail.com>
2022-02-27 01:49:08 -0500

    In VideoList component: created clickHandler function, mapped filtered videos into a list and added clickHandler to each video, then passed clicked video event to parent App’s handleVideoChange event, and successfully tested that active video/state changes

--- src/components/VideoList/VideoList.js
+++ src/components/VideoList/VideoList.js
@@ -1,13 +1,25 @@
 import './VideoList.scss';
 
 const VideoList = (props) => {
-    const {filteredVideos, handleVideoChange} = props;
+    const { filteredVideos, handleVideoChange } = props;
+
+    const clickHandler = (event, clickedVideo) => {
+        event.preventDefault();
+        handleVideoChange(clickedVideo);
+    }
     return (
-
-        <>
-            <p>{filteredVideos[1].title}</p>
-        </>
-
+        <ul className='video-list'>
+            {filteredVideos.map(video => (
+                <li key={video.id} className='video-list__item'>
+                    <a href='/'
+                        onClick={event => { clickHandler(event, video) }}>
+                        <div>
+                            <p>{video.title}</p>
+                        </div>
+                    </a>
+                </li>
+            ))}
+        </ul>
     );
 };
 
