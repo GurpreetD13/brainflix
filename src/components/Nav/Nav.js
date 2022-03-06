4817d9f68640ab4604bee6dc2d4eae7afbf1150a
Gurpreet <gurpreeet.dhaliwal13@gmail.com>
2022-03-06 17:35:28 -0500

    Added NavLinks to Nav bar component for site Logo to go to Home, and Upload button to go to Upload page~

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,15 +1,20 @@
 import './Nav.scss';
+import { NavLink } from 'react-router-dom';
 import logo from '../../assets/logos/BrainFlix-logo.svg';
 
 const Nav = () => {
     return (
         <nav className='nav'>
-            <img className='nav__logo' src={logo} alt="BrainFlix logo" />
+            <NavLink to='/'>
+                <img className='nav__logo' src={logo} alt="BrainFlix logo" />
+            </NavLink>
             <form>
                 <input className='nav__search' type="text" placeholder='Search' />
                 <div className='avatar nav__avatar nav__avatar--mobile'></div>
             </form>
-            <button className='nav__button'>UPLOAD</button>
+            <NavLink to='/upload'>
+                <button className='nav__button'>UPLOAD</button>
+            </NavLink>
             <div className='avatar nav__avatar nav__avatar--tablet'></div>
         </nav>
     );
