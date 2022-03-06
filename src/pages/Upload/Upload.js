0d1d236024de66f63a266ced7eea352f19a04042
Gurpreet <gurpreeet.dhaliwal13@gmail.com>
2022-03-06 14:30:05 -0500

    Created html for Upload page including a form for uploading

--- src/pages/Upload/Upload.js
+++ src/pages/Upload/Upload.js
@@ -1,12 +1,46 @@
 import './Upload.scss';
+import uploadImage from '../../assets/images/Upload-video-preview.jpg'
+
 
 const Upload = () => {
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+    
+        alert("Your video has been successfully uploaded!");
+
+        //redirect to home
+    }
+
     return (
 
+        <main className='main'>
 
-        <h1>Upload</h1>
+            <h1>Upload Video</h1>
 
-    )
-}
+            <form onSubmit={handleSubmit}>
+
+                <div>
+                    <h2>VIDEO THUMBNAIL</h2>
+                    {/* <img src={uploadImage} alt="Bike handlebars from biker's point of view while biking" /> */}
+                </div>
+
+                <div>
+                    <label htmlFor="">TITLE YOUR VIDEO</label>
+                    <input type="text" placeholder='Add a title to your video'/>
+
+                    <label htmlFor="">ADD A VIDEO DESCRIPTION</label>
+                    <textarea name="" placeholder='Add a description to your video'></textarea>
+                </div>
+
+                <div>
+                    <button>PUBLISH</button>
+                    <button disabled="disabled">CANCEL</button>
+                </div>
+            </form>
+        </main>
+    );
+};
 
 export default Upload;
