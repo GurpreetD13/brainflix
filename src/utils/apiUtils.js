d901adefcf9b4bf5a92a7e646d930437976fc5c5
Gurpreet <gurpreeet.dhaliwal13@gmail.com>
2022-03-06 02:19:25 -0500

    Created apiUtils file to make reusable functions for axios requests to getAllVideosArray and getSingleVideoDetails object from api

---
+++ src/utils/apiUtils.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+
+const apiBaseUrl = 'https://project-2-api.herokuapp.com';
+
+const apiKeyString = '?api_key=' + 'f76f19d5-1908-4af7-a8b7-ecbd1dba973c';
+
+
+const apiUtils = {
+
+    // to GET array/list of All videos use: 
+
+    getAllVideosArray: () => axios.get(`${apiBaseUrl}/videos/${apiKeyString}`),
+
+    // to GET Single detailed video object using the videoID use: 
+
+    getSingleVideoDetails: (videoId) => axios.get(`${apiBaseUrl}/videos/${videoId}${apiKeyString}`)
+};
+
+export default apiUtils;
