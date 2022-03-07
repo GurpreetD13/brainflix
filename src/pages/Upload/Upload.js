3cee8e3fb2ecb2557cda8d00be739ad22452a7f4
Gurpreet <gurpreeet.dhaliwal13@gmail.com>
2022-03-06 21:17:04 -0500

    Styled Upload page and added extra spaced line within all Sass files at end of each rule set as per feedback from sprint-1 to make it easier for others to read

--- src/pages/Upload/Upload.js
+++ src/pages/Upload/Upload.js
@@ -4,39 +4,35 @@ import uploadImage from '../../assets/images/Upload-video-preview.jpg'
 
 const Upload = (routerProps) => {
 
-
     const handleSubmit = (event) => {
         event.preventDefault();
-        alert("Your video has been successfully uploaded!");
-
+        alert("Your video has been successfully oploaded!");
         // Redirect to Home page ('/') after form submitted, using history routerProp being passed down
         routerProps.history.push('/');
     }
 
     return (
-
         <main className='main'>
+            <h1 className='upload__heading'>Upload Video</h1>
 
-            <h1>Upload Video</h1>
+            <form onSubmit={handleSubmit} className='upload__form'>
 
-            <form onSubmit={handleSubmit}>
+                <div className='upload-form__top-container'>
+                    <div className='upload-form__thumbnail-container'>
+                        <h2 className='thumbnail__label'>VIDEO THUMBNAIL</h2>
+                        <img className='thumbnail__image' src={uploadImage} alt="Bike handlebars from biker's point of view while biking" />
+                    </div>
+                    <div className='upload-form__inputs-container'>
+                        <label htmlFor="userVideoTitle">TITLE YOUR VIDEO</label>
+                        <input name="userVideoTitle" type="text" placeholder='Add a title to your video' />
 
-                <div>
-                    <h2>VIDEO THUMBNAIL</h2>
-                    {/* <img src={uploadImage} alt="Bike handlebars from biker's point of view while biking" /> */}
+                        <label htmlFor="userVideoDescription">ADD A VIDEO DESCRIPTION</label>
+                        <textarea name="userVideoDescription" rows="5" placeholder='Add a description to your video'></textarea>
+                    </div>
                 </div>
-
-                <div>
-                    <label htmlFor="">TITLE YOUR VIDEO</label>
-                    <input type="text" placeholder='Add a title to your video'/>
-
-                    <label htmlFor="">ADD A VIDEO DESCRIPTION</label>
-                    <textarea name="" placeholder='Add a description to your video'></textarea>
-                </div>
-
-                <div>
-                    <button>PUBLISH</button>
-                    <button disabled="disabled">CANCEL</button>
+                <div className='upload-form__buttons-container'>
+                    <button className='button--publish'>PUBLISH</button>
+                    <button className='button--cancel' disabled="disabled">CANCEL</button>
                 </div>
             </form>
         </main>
