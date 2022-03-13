57a22dcd895e59545dffaabf2332851f6fe68841
Gurpreet <gurpreeet.dhaliwal13@gmail.com>
2022-03-13 03:55:09 -0400

    Successfully switched API from previous mock API to custom built API server

--- src/utils/apiUtils.js
+++ src/utils/apiUtils.js
@@ -1,20 +1,18 @@
 import axios from "axios";
 
 
-const apiBaseUrl = 'https://project-2-api.herokuapp.com';
-
-const apiKeyString = '?api_key=f76f19d5-1908-4af7-a8b7-ecbd1dba973c';
+const apiBaseUrl = 'http://localhost:8080';
 
 
 const apiUtils = {
 
     // to GET array/list of All videos from API use: 
 
-    getAllVideosArray: () => axios.get(`${apiBaseUrl}/videos/${apiKeyString}`),
+    getAllVideosArray: () => axios.get(`${apiBaseUrl}/videos/`),
 
     // to GET Single detailed video object using the videoID from API use: 
 
-    getSingleVideoDetails: (videoId) => axios.get(`${apiBaseUrl}/videos/${videoId}${apiKeyString}`)
+    getSingleVideoDetails: (videoId) => axios.get(`${apiBaseUrl}/videos/${videoId}`)
 };
 
 export default apiUtils;
